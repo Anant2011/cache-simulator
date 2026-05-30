44c16c764567755c3a9232ec733cdfb0fb8e49d0
Anant2011 <63188047+Anant2011@users.noreply.github.com>
2026-05-31 00:51:45 +0530

    Update cachelab.h

--- cachelab.h
+++ cachelab.h
@@ -1,6 +1,4 @@
-/* 
- * cachelab.h - Prototypes for Cache Lab helper functions
- */
+
 
 #ifndef CACHELAB_TOOLS_H
 #define CACHELAB_TOOLS_H
@@ -16,10 +14,7 @@ typedef struct trans_func{
   unsigned int num_evictions;
 } trans_func_t;
 
-/* 
- * printSummary - This function provides a standard way for your cache
- * simulator * to display its final hit and miss statistics
- */ 
+
 void printSummary(int hits,  /* number of  hits */
 				  int misses, /* number of misses */
 				  int evictions); /* number of evictions */
